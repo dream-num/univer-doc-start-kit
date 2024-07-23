2ecd9d9ef8552ea0f45c0a9a8ac8fc1decce5347
hexf00 <hexf00@gmail.com>
2024-07-23 17:42:53 +0800

    feat: update to v0.2.4

--- src/locale.ts
+++ src/locale.ts
@@ -1,16 +1,22 @@
-import { LocaleType } from '@univerjs/core'
+import { LocaleType, Tools } from '@univerjs/core'
 import UniverDesignEnUS from '@univerjs/design/locale/en-US'
 import UniverDocsUIEnUS from '@univerjs/docs-ui/locale/en-US'
 import UniverSheetsEnUS from '@univerjs/sheets/locale/en-US'
 import UniverSheetsUIEnUS from '@univerjs/sheets-ui/locale/en-US'
 import UniverUiEnUS from '@univerjs/ui/locale/en-US'
+import DocsHyperLinkUIEnUS from '@univerjs/docs-hyper-link-ui/locale/en-US'
+import DrawingUIEnUS from '@univerjs/drawing-ui/locale/en-US'
+import DocsDrawingUIEnUS from '@univerjs/docs-drawing-ui/locale/en-US'
 
 export const locales = {
-  [LocaleType.EN_US]: {
-    ...UniverSheetsEnUS,
-    ...UniverDocsUIEnUS,
-    ...UniverSheetsUIEnUS,
-    ...UniverUiEnUS,
-    ...UniverDesignEnUS,
-  },
+  [LocaleType.EN_US]: Tools.deepMerge(
+    UniverSheetsEnUS,
+    UniverDocsUIEnUS,
+    UniverSheetsUIEnUS,
+    UniverUiEnUS,
+    UniverDesignEnUS,
+    DocsHyperLinkUIEnUS,
+    DrawingUIEnUS,
+    DocsDrawingUIEnUS,
+  ),
 }
--- src/setup-univer.ts
+++ src/setup-univer.ts
@@ -1,13 +1,24 @@
 import '@univerjs/design/lib/index.css'
 import '@univerjs/ui/lib/index.css'
+import '@univerjs/docs-hyper-link-ui/lib/index.css'
+import '@univerjs/drawing-ui/lib/index.css'
+import '@univerjs/docs-drawing-ui/lib/index.css'
 
-import { LocaleType, LogLevel, Univer, UniverInstanceType } from '@univerjs/core'
+import type { DocumentDataModel, IDocumentData } from '@univerjs/core'
+import { DocumentFlavor, LocaleType, LogLevel, Univer, UniverInstanceType } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
 import { UniverRenderEnginePlugin, ptToPixel } from '@univerjs/engine-render'
 import { UniverUIPlugin } from '@univerjs/ui'
+import { UniverDocsHyperLinkPlugin } from '@univerjs/docs-hyper-link'
+import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
+import { UniverDrawingPlugin } from '@univerjs/drawing'
+import { UniverDrawingUIPlugin } from '@univerjs/drawing-ui'
+import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing'
+import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
+
 import { FUniver } from '@univerjs/facade'
 import { locales } from './locale'
 import { convertPureStringToUDM } from './libs/convertPureStringToUDM'
@@ -15,7 +26,7 @@ import { convertPureStringToUDM } from './libs/convertPureStringToUDM'
 export function setupUniver() {
   const univer = new Univer({
     theme: defaultTheme,
-    locale: LocaleType.ZH_CN,
+    locale: LocaleType.EN_US,
     logLevel: LogLevel.VERBOSE,
     locales,
   })
@@ -26,6 +37,7 @@ export function setupUniver() {
   univer.registerPlugin(UniverUIPlugin, {
     container: 'univer',
     header: true,
+    footer: false,
   })
   univer.registerPlugin(UniverDocsPlugin)
   univer.registerPlugin(UniverDocsUIPlugin, {
@@ -37,9 +49,24 @@ export function setupUniver() {
     },
   })
 
-  univer.createUnit(UniverInstanceType.UNIVER_DOC, {
+  // hyper link plugins
+  univer.registerPlugin(UniverDocsHyperLinkPlugin)
+  univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
+
+  // drawing plugins
+  univer.registerPlugin(UniverDrawingPlugin)
+  univer.registerPlugin(UniverDrawingUIPlugin)
+  univer.registerPlugin(UniverDocsDrawingPlugin)
+  univer.registerPlugin(UniverDocsDrawingUIPlugin)
+
+  univer.createUnit<IDocumentData, DocumentDataModel>(UniverInstanceType.UNIVER_DOC, {
     body: convertPureStringToUDM(''),
+    drawings: {},
+    drawingsOrder: [],
+    headers: {},
+    footers: {},
     documentStyle: {
+      documentFlavor: DocumentFlavor.TRADITIONAL, // enable header and footer
       pageSize: {
         width: ptToPixel(595),
         height: ptToPixel(842),
@@ -52,6 +79,12 @@ export function setupUniver() {
         vertexAngle: 0,
         centerAngle: 0,
       },
+      defaultHeaderId: '',
+      defaultFooterId: '',
+      evenPageHeaderId: '',
+      evenPageFooterId: '',
+      firstPageHeaderId: '',
+      firstPageFooterId: '',
     },
   })
 
