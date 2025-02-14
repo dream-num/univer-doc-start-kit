ed7876ccc2e21d1073d5bf8e21f9cf5e9b65d723
hexf00 <hexf00@gmail.com>
2025-02-15 00:16:53 +0800

    feat: update to 0.6.0

--- src/setup-univer.ts
+++ src/setup-univer.ts
@@ -1,98 +1,50 @@
-import '@univerjs/design/lib/index.css'
-import '@univerjs/ui/lib/index.css'
-import '@univerjs/docs-hyper-link-ui/lib/index.css'
-import '@univerjs/drawing-ui/lib/index.css'
-import '@univerjs/docs-drawing-ui/lib/index.css'
+import '@univerjs/presets/lib/styles/preset-docs-core.css'
+import '@univerjs/presets/lib/styles/preset-docs-thread-comment.css'
+import '@univerjs/presets/lib/styles/preset-docs-drawing.css'
+import '@univerjs/presets/lib/styles/preset-docs-hyper-link.css'
 
-import type { DocumentDataModel, IDocumentData } from '@univerjs/core'
-import { DocumentFlavor, LocaleType, LogLevel, Univer, UniverInstanceType } from '@univerjs/core'
-import { defaultTheme } from '@univerjs/design'
-import { UniverDocsPlugin } from '@univerjs/docs'
-import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
-import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
-import { UniverRenderEnginePlugin, ptToPixel } from '@univerjs/engine-render'
-import { UniverUIPlugin } from '@univerjs/ui'
-import { UniverDocsHyperLinkPlugin } from '@univerjs/docs-hyper-link'
-import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
-import { UniverDrawingPlugin } from '@univerjs/drawing'
-import { UniverDrawingUIPlugin } from '@univerjs/drawing-ui'
-import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing'
-import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui'
+import { LocaleType, LogLevel, createUniver, defaultTheme, merge } from '@univerjs/presets'
 
-import { FUniver } from '@univerjs/facade'
-import { locales } from './locale'
-import { convertPureStringToUDM } from './libs/convertPureStringToUDM'
+import { UniverDocsCorePreset } from '@univerjs/presets/preset-docs-core'
+import docsCoreEnUs from '@univerjs/presets/preset-docs-core/locales/en-US'
+
+import { UniverDocsDrawingPreset } from '@univerjs/presets/preset-docs-drawing'
+import docsDrawingEnUs from '@univerjs/presets/preset-docs-drawing/locales/en-US'
+
+import { UniverDocsHyperLinkPreset } from '@univerjs/presets/preset-docs-hyper-link'
+import docsHyperLinkEnUs from '@univerjs/presets/preset-docs-hyper-link/locales/en-US'
+
+import { UniverDocsThreadCommentPreset } from '@univerjs/presets/preset-docs-thread-comment'
+import docsThreadCommentEnUs from '@univerjs/presets/preset-docs-thread-comment/locales/en-US'
 
 export function setupUniver() {
-  const univer = new Univer({
-    theme: defaultTheme,
+  const { univerAPI } = createUniver({
     locale: LocaleType.EN_US,
-    logLevel: LogLevel.VERBOSE,
-    locales,
-  })
-
-  // core plugins
-  univer.registerPlugin(UniverRenderEnginePlugin)
-  univer.registerPlugin(UniverFormulaEnginePlugin)
-  univer.registerPlugin(UniverUIPlugin, {
-    container: 'univer',
-    header: true,
-    footer: false,
-  })
-  univer.registerPlugin(UniverDocsPlugin)
-  univer.registerPlugin(UniverDocsUIPlugin, {
-    container: 'univerdoc',
-    layout: {
-      docContainerConfig: {
-        innerLeft: false,
-      },
+    locales: {
+      [LocaleType.EN_US]: merge(
+        {},
+        docsCoreEnUs,
+        docsDrawingEnUs,
+        docsHyperLinkEnUs,
+        docsThreadCommentEnUs,
+      ),
     },
+    logLevel: LogLevel.VERBOSE,
+    theme: defaultTheme,
+    presets: [
+      UniverDocsCorePreset({
+        container: 'univer',
+        header: true,
+        footer: true,
+      }),
+      UniverDocsDrawingPreset(),
+      UniverDocsHyperLinkPreset(),
+      UniverDocsThreadCommentPreset(),
+    ],
+    plugins: [],
   })
 
-  // hyper link plugins
-  univer.registerPlugin(UniverDocsHyperLinkPlugin)
-  univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
-
-  // drawing plugins
-  univer.registerPlugin(UniverDrawingPlugin)
-  univer.registerPlugin(UniverDrawingUIPlugin)
-  univer.registerPlugin(UniverDocsDrawingPlugin)
-  univer.registerPlugin(UniverDocsDrawingUIPlugin)
-
-  setTimeout(() => {
-    univer.createUnit<IDocumentData, DocumentDataModel>(UniverInstanceType.UNIVER_DOC, {
-      body: convertPureStringToUDM(''),
-      drawings: {},
-      drawingsOrder: [],
-      headers: {},
-      footers: {},
-      tableSource: {},
-      documentStyle: {
-        documentFlavor: DocumentFlavor.TRADITIONAL, // enable header and footer
-        pageSize: {
-          width: ptToPixel(595),
-          height: ptToPixel(842),
-        },
-        marginTop: ptToPixel(50),
-        marginBottom: ptToPixel(50),
-        marginRight: ptToPixel(40),
-        marginLeft: ptToPixel(40),
-        renderConfig: {
-          vertexAngle: 0,
-          centerAngle: 0,
-        },
-        defaultHeaderId: '',
-        defaultFooterId: '',
-        evenPageHeaderId: '',
-        evenPageFooterId: '',
-        firstPageHeaderId: '',
-        firstPageFooterId: '',
-      },
-    })
-  }, 0)
-
-  const univerAPI = FUniver.newAPI(univer)
-
+  univerAPI.createUniverDoc({})
   univerAPI.getActiveDocument()?.appendText('Hello World!')
 
   return univerAPI
